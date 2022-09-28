919d84fcccb8f82ee5447f8c42faefcc2f863028
進義 周 <charles-chou@hotmail.com>
2022-09-28 10:43:02 +0800

    第一版作業加註解

--- SchemaNotes_11168/Models/DA_schemaNotesColumn.cs
+++ SchemaNotes_11168/Models/DA_schemaNotesColumn.cs
@@ -7,15 +7,31 @@ using SchemaNotes_11168.Models;
 
 namespace SchemaNotes_11168.Models
 {
+    /// <summary>
+    /// get the details of schemaNotesColumn from db via ado.net connection
+    /// </summary>
     public class DA_schemaNotesColumn
     {
+        /// <summary>
+        ///  method to get the details of schemaColumn via ado.net and datareader 
+        /// </summary>
+        /// <param name="connStrings"></param>
+        /// <returns>List</returns>
         public List<DO_schemaNotesColumn> GetColumns(string connStrings)
         {
+           ///<summary>
+           ///Declare  a object of List to receive the results of queried
+           /// </summary>
             List<DO_schemaNotesColumn> SNCList = new List<DO_schemaNotesColumn>();
+            ///<summary>
+            /// sql connection  via ado.net and datareader 
+            ///</summary>
             using (SqlConnection conn = new SqlConnection(connStrings))
             {
-                conn.Open();
-                string commandText = "SELECT SO.name AS [物件名稱],SC.name AS[欄位名稱],     SE1.value AS[欄位說明],  ISC.DATA_TYPE + '(' + CONVERT(VARCHAR, ISC.CHARACTER_MAXIMUM_LENGTH) + ')' AS[資料型態],"
+                #region queries of sql 
+                string commandText = "SELECT SO.name AS [物件名稱],SC.name AS[欄位名稱],   " 
+                   + "  SE1.value AS[欄位說明], "
+                    + " CASE WHEN ISC.CHARACTER_MAXIMUM_LENGTH IS NULL THEN ISC.DATA_TYPE ELSE  (ISC.DATA_TYPE + '(' + CONVERT(VARCHAR, ISC.CHARACTER_MAXIMUM_LENGTH) + ')') END AS[資料型態],"
                    + "CASE  WHEN ISK.CONSTRAINT_NAME IS NULL THEN 0   ELSE 1   END AS[主鍵],  ISC.IS_NULLABLE AS[不為NULL],ISC.COLUMN_DEFAULT AS[預設值], SE.value AS[備註]"
                    + "FROM sys.columns AS SC"
                    + " LEFT JOIN sys.objects SO ON SO.object_id = SC.object_id"
@@ -25,9 +41,11 @@ namespace SchemaNotes_11168.Models
                    + " LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS ISK ON ISK.TABLE_NAME = SO.name  AND SC.name = ISK.COLUMN_NAME "
                    + $"WHERE OBJECT_NAME(SO.object_id) IN "
                       + "(SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES) ORDER BY SC.name;";
-            using (SqlCommand command = new SqlCommand(commandText, conn))
-                {
-                    SqlDataReader dataReader = command.ExecuteReader();
+                #endregion
+                SqlCommand command = new SqlCommand(commandText, conn);
+                conn.Open();
+                #region  add all queried result to List  and return the list
+                SqlDataReader dataReader = command.ExecuteReader();
                     while (dataReader.Read())
                     {
                         DO_schemaNotesColumn DOSNC = new DO_schemaNotesColumn
@@ -43,12 +61,10 @@ namespace SchemaNotes_11168.Models
                             columnRemark = dataReader["備註"].ToString()
                         };
                         SNCList.Add(DOSNC);
-
                     }
-                }
-                return SNCList;
+                return SNCList; 
+                #endregion
             }
-
         }
     }
 }
--- SchemaNotes_11168/Models/DO_schemaNotesTable.cs
+++ SchemaNotes_11168/Models/DO_schemaNotesTable.cs
@@ -6,25 +6,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SchemaNotes_11168.Models
 {
+    /// <summary>
+    ///  class for fields of  SchemaNotes for table
+    /// </summary>
     public class DO_schemaNotesTable
     {
-        [Display(Name = "物件名稱")]
+        #region fields of details of table for SchemaNotes
         public string tableName { get; set; }
-        [Display(Name = "物件說明")]
         public string tableMSDescription { get; set; }
-        [Display(Name = "物件類型")]
         public string tableType { get; set; }
-        [Display(Name = "結構描述名稱")]
         public string tableStruct { get; set; }
-        [Display(Name = "物件創建日期")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = false)]
         public DateTime tableCreateTime { get; set; }
-        [Display(Name = "物件修改日期")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = false)]
         public DateTime tableModifiedTime { get; set; }
-        [Display(Name = "備註")]
         public string tableRemark { get; set; }
-        [Display(Name = "資料筆數")]
         public int tableRows { get; set; }
+        #endregion
     }
 }
