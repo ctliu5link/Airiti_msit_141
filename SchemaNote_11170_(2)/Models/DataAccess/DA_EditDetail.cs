cbad65b0f7e169f768b7d3ba25f4f687ebd3ba97
進義 周 <charles-chou@hotmail.com>
2022-10-13 08:04:00 +0800

    dd

---
+++ SchemaNote_11170_(2)/Models/DataAccess/DA_EditDetail.cs
@@ -0,0 +1,19 @@
+﻿using SchemaNote_11170__2_.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Windows.Forms;
+
+namespace SchemaNote_11170__2_.Models.DataAccess
+{
+    public class DA_EditDetail
+    {
+        public void Edit_TableDetail(VM_ShowData vModel)
+        {
+            
+        }
+        
+    }
+}
